e6f66145e1b7a9323f0fec8d726f2859ca8f01c4
Alexander Khalkuziev <akhalkuziev@mirantis.com>
2013-02-22 18:43:13 +0400

    Add annotations

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'akhalkuziev@mirantis.com'
 license          'All rights reserved'
 description      'Installs/Configures gerrit'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 %w{ java mysql }.each do |dep|
   depends dep
