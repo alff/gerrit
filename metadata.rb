ba91c77ad741d5d36c296a624137f0e24f6c975a
John Dewey <john@dewey.ws>
2013-05-03 17:12:35 -0700

    Added AT&T standards

--- metadata.rb
+++ metadata.rb
@@ -1,11 +1,14 @@
-name             'gerrit'
-maintainer       'Mirantis, IT'
-maintainer_email 'akhalkuziev@mirantis.com'
-license          'All rights reserved'
-description      'Installs/Configures gerrit'
+name             "gerrit"
+maintainer       "AT&T Services, Inc."
+maintainer_email "cookbooks@lists.tfoundry.com"
+license          "Apache 2.0"
+description      "Installs/Configures gerrit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          "1.2.0"
 
-%w{ java mysql }.each do |dep|
-  depends dep
-end
+recipe           "gerrit::server", "Installs/Configures gerrit"
+
+supports         ubuntu
+
+depends          java
+depends          mysql
